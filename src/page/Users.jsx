cf55acacd15daea0077e16486a36ef6bc8ef7890
Đỗ Thành Long <118795350+DoThanhLong20187249@users.noreply.github.com>
2024-04-15 17:34:06 +0700

    data-grid-table

--- src/page/Users.jsx
+++ src/page/Users.jsx
@@ -1,8 +1,15 @@
+import DataTable from "../componets/dataTable/DataTable";
+
+
 
 const Users = () => {
     return (
-        <div className="users">
-            Users
+        <div className="users-container">
+            <div className="infor">
+                <h1>Users</h1>
+                <button className="btn">Add User</button>
+            </div>
+            <DataTable/>
         </div>
     );
 };
